6e1aec2cd378cfb93b2d33ea5fa38ab8589939f8
Diman <dimanozdrin1999@gmail.com>
2023-11-03 16:20:48 +0300

    PP

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -7,7 +7,7 @@ import jm.task.core.jdbc.model.User;
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    private final UserDao u1 = new UserDaoJDBCImpl();
+    private final UserDao u1 = new UserDaoHibernateImpl();
     public void createUsersTable() {
         u1.createUsersTable();
     }
